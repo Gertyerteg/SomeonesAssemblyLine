b4237c2bd114261d0a8271261b376e8b37a158ae
Spencer C <SpencerChang2001@Gmail.com>
2018-03-15 00:57:15 -0700

    Initial Commit

---
+++ SAL/SAL/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Someone's Assembly Line
+ * 
+ * Design your own factory! Manufactor different components through an assembly line
+ * to build up your wealth.
+ */
+using System;
+
+namespace SAL
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
